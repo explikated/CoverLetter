39dc8a05c5aafe2fb8cfcd99cbd0a80d8c426c90
Kate <calculated.mayhem@gmail.com>
2014-09-20 22:23:05 -0700

    configuring AWS. Now working on local machine

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -5,11 +5,10 @@ class Piece < ActiveRecord::Base
     attachment_size: { less_than: 5.megabytes }
 	
   # This method associates the attribute ":photo" with a file attachment
-  has_attached_file :photo, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file :photo,
+    :storage => :s3,
+	:bucket => 'kdrams-rb-coverletter',
+	:s3_credentials => "#{Rails.root}/config/s3.yml"
   
   def next
     Piece.find(self.id + 1)
